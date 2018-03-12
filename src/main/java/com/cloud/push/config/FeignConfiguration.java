801da526eb7eb943f16c417582934f23fa070c5f
yingmingbo <124599792@qq.com>
2018-03-12 10:43:25 +0800

    由jhispter生成微服务

---
+++ src/main/java/com/cloud/push/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.cloud.push.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.cloud.push")
+public class FeignConfiguration {
+
+}
---
+++ src/main/java/com/cloud/push/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cloud.push.config.audit;
