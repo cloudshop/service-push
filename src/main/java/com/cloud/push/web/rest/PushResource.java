28d82b20b0838a38b8d2357210553e44644dd5ef
wgw <756898059@qq.com>
2018-04-26 16:32:23 +0800

    推送

--- src/main/java/com/cloud/push/web/rest/PushResource.java
+++ src/main/java/com/cloud/push/web/rest/PushResource.java
@@ -22,8 +22,11 @@ import com.cloud.push.service.MessageQueryService;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,9 +48,10 @@ public class PushResource {
     private final Logger log = LoggerFactory.getLogger(PushResource.class);
 
     private static final String ENTITY_NAME = "push";
+    
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
-    @PostMapping("/push")
-    @Timed
     /**
      * @author 逍遥子
      * @email 756898059@qq.com
@@ -57,6 +61,8 @@ public class PushResource {
      * @return PushResult
      * @throws Exception
      */
+    @PostMapping("/push")
+    @Timed
     public @ResponseBody PushResult sendPush(@RequestBody Push push) throws Exception {
         log.debug("REST request to send push : {}", push);
         if (push.getId() != null) {
@@ -85,88 +91,46 @@ public class PushResource {
     }
     
     /**
-     * POST  /messages : Create a new message.
-     *
-     * @param messageDTO the messageDTO to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new messageDTO, or with status 400 (Bad Request) if the message has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     
-    @PostMapping("/messages")
-    @Timed
-    public ResponseEntity<MessageDTO> createMessage(@RequestBody MessageDTO messageDTO) throws URISyntaxException {
-        log.debug("REST request to save Message : {}", messageDTO);
-        if (messageDTO.getId() != null) {
-            throw new BadRequestAlertException("A new message cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        MessageDTO result = messageService.save(messageDTO);
-        return ResponseEntity.created(new URI("/api/messages/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }*/
-
-    /**
-     * PUT  /messages : Updates an existing message.
-     *
-     * @param messageDTO the messageDTO to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated messageDTO,
-     * or with status 400 (Bad Request) if the messageDTO is not valid,
-     * or with status 500 (Internal Server Error) if the messageDTO couldn't be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     
-    @PutMapping("/messages")
-    @Timed
-    public ResponseEntity<MessageDTO> updateMessage(@RequestBody MessageDTO messageDTO) throws URISyntaxException {
-        log.debug("REST request to update Message : {}", messageDTO);
-        if (messageDTO.getId() == null) {
-            return createMessage(messageDTO);
-        }
-        MessageDTO result = messageService.save(messageDTO);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, messageDTO.getId().toString()))
-            .body(result);
-    }*/
-
-    /**
-     * GET  /messages : get all the messages.
-     *
-     * @param pageable the pagination information
-     * @param criteria the criterias which the requested entities should match
-     * @return the ResponseEntity with status 200 (OK) and the list of messages in body
-     
-    @GetMapping("/messages")
-    @Timed
-    public ResponseEntity<List<MessageDTO>> getAllMessages(MessageCriteria criteria, Pageable pageable) {
-        log.debug("REST request to get Messages by criteria: {}", criteria);
-        Page<MessageDTO> page = messageQueryService.findByCriteria(criteria, pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/messages");
-        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
-    }*/
-
-    /**
-     * GET  /messages/:id : get the "id" message.
-     *
-     * @param id the id of the messageDTO to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the messageDTO, or with status 404 (Not Found)
-     
-    @GetMapping("/messages/{id}")
-    @Timed
-    public ResponseEntity<MessageDTO> getMessage(@PathVariable Long id) {
-        log.debug("REST request to get Message : {}", id);
-        MessageDTO messageDTO = messageService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(messageDTO));
-    }*/
-
-    /**
-     * DELETE  /messages/:id : delete the "id" message.
-     *
-     * @param id the id of the messageDTO to delete
-     * @return the ResponseEntity with status 200 (OK)
+     * 根据用户id推送
+     * @author 逍遥子
+     * @email 756898059@qq.com
+     * @date 2018年4月26日
+     * @version 1.0
+     * @param userid
+     * @param content
+     */
+    @PostMapping("/push/user/{userid}")
+    public void sendPushByUserid(@PathVariable("userid") String userid,@RequestBody String content) {
+    	ValueOperations<String, String> opsForValue = redisTemplate.opsForValue();
+    	String registrationID = opsForValue.get("gongrong_jpush_registrationID_userid_"+userid);
+    	PushClient pushClient = new PushClient("7b86cdd203328e704718bce9", "bd5ea486e324b235c89b6340");
+    	try {
+			PushResult sendPush = pushClient.sendPush(PushPayload.newBuilder()
+					.setPlatform(Platform.android_ios())
+			    	.setNotification(Notification.alert(content))
+			    	.setAudience(Audience.registrationId(registrationID)).build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
     
-    @DeleteMapping("/messages/{id}")
-    @Timed
-    public ResponseEntity<Void> deleteMessage(@PathVariable Long id) {
-        log.debug("REST request to delete Message : {}", id);
-        messageService.delete(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
-    } */
+    /**
+     * 群退
+     * @author 逍遥子
+     * @email 756898059@qq.com
+     * @date 2018年4月26日
+     * @version 1.0
+     * @param userid
+     * @param content
+     */
+    @PostMapping("/push/all")
+    public void sendPushToAll(@RequestBody String content) {
+    	PushClient pushClient = new PushClient("7b86cdd203328e704718bce9", "bd5ea486e324b235c89b6340");
+    	try {
+    		pushClient.sendPush(PushPayload.alertAll(content));
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
 }
